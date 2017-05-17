b0acb1d154fd1029d54053e8a233db6b3ac4d5f0
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-05-17 15:26:09 -0600

    renamed files to js specification

---
+++ test/book-parser-spec.js
@@ -0,0 +1,49 @@
+import {assert, expect} from 'chai';
+import moment from 'moment';
+import {parseLine, Checkout} from '../src/bookParser.js';
+
+describe('A parse of a simple file', () => {
+   it('should parse a line with "~" separators', () => {
+      const result = parseLine('Beth Brown~The Leftovers~2013-03-31')
+      //expect(result).to.be.a('Checkout');
+      expect(result.name).to.be.eql('Beth Brown');
+      expect(result.title).to.be.eql('The Leftovers');
+      assert(result.date.isSame(moment('2013-03-31')));
+   });
+   it('should parse another line with different content', () => {
+      const result = parseLine('Daniel Hinojosa~Functional Programming~2015-05-11')
+      //expect(result).to.be.a('Checkout');
+      expect(result.name).to.be.eql('Daniel Hinojosa');
+      expect(result.title).to.be.eql('Functional Programming');
+      assert(result.date.isSame(moment('2015-05-11')));
+   });
+   it('should parse line with 2 number of items', () => {
+      try {
+        parseLine('Daniel Hinojosa~Functional Programming');
+        assert(false);
+      } catch (e) {
+        expect(e).to.be.an('error');
+        expect(e.message).to.be.eql('Invalid number of tokens');
+      }
+   });
+   it('should parse line with 4 number of items', () => {
+      try {
+        parseLine('Daniel Hinojosa~Functional~Programming~2015-05-11');
+        assert(false);
+      } catch (e) {
+        expect(e).to.be.an('error');
+        expect(e.message).to.be.eql('Invalid number of tokens');
+      }
+   });
+});
+describe('A checkout and the penalty', () => {
+   it('todays date and checkout date are the same', () => {
+      const checkout = new Checkout(null, null, moment("2017-02-01"));
+      expect(checkout.penalty(moment('2017-02-01'), 10)).to.be.eql(0);
+   });
+   it('todays date and checkout date are the same', () => {
+      const checkout = new Checkout(null, null, moment("2017-02-01"));
+      expect(checkout.penalty(moment('2017-03-01'), 10)).to.be.eql(0);
+   });
+
+});
